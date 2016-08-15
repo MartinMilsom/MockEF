6129892f7ec625040a10d09be504366acf4f9574
MartinMilsom <martin.milsom@sportsnewmedia.com>
2016-08-15 17:19:18 +0100

    Revert "Remove Rhino dependency. Use Castle DynamicProxy instead"
    This reverts commit b301486118f5161fea3673ff8be58001b2392109.

--- MockEF.Tests/MockEfDbContextTests.cs
+++ MockEF.Tests/MockEfDbContextTests.cs
@@ -9,12 +9,12 @@ namespace MockEF.Tests
     public class MockEfDbContextTests
     {
         [TestMethod]
-        public void SeededDataIsAdded()
+        public void SeededDataIsFound()
         {
-            var builder = new Double<IMyContext>()
+            var builder = new ContextBuilder<IMyContext>()
                 .Setup(x => x.Authors, new List<Author> { new Author { Name = "bob" } })
                 .Setup(x => x.Books, new List<Book>());
-            var target = builder.Build();
+            var target = builder.GetContext();
 
             var data = target.Authors.Where(x => x.Name == "bob");
 
@@ -22,12 +22,12 @@ namespace MockEF.Tests
         }
 
         [TestMethod]
-        public void AddNewData()
+        public void DynamicallyAddedData_StaysPersisted()
         {
-            var builder = new Double<IMyContext>()
+            var builder = new ContextBuilder<IMyContext>()
                 .Setup(x => x.Authors, new List<Author>())
                 .Setup(x => x.Books, new List<Book>());
-            var target = builder.Build();
+            var target = builder.GetContext();
 
             Assert.AreEqual(0, target.Authors.Count());
             target.Authors.Add(new Author { Name = "bob" });
@@ -36,11 +36,11 @@ namespace MockEF.Tests
         }
 
         [TestMethod]
-        public void FindData()
+        public void FindOnSeededData_ReturnsValue()
         {
-            var target = new Double<IMyContext>()
+            var target = new ContextBuilder<IMyContext>()
                 .Setup(x => x.Authors, new List<Author> { new Author { Id = 1, Name = "bob" }, new Author { Id = 2, Name = "dave" } })
-                .Setup(x => x.Books).Build();
+                .Setup(x => x.Books).GetContext();
 
             Assert.AreEqual(2, target.Authors.Count());
 
@@ -51,16 +51,16 @@ namespace MockEF.Tests
         }
 
         [TestMethod]
-        public void FindData_WithCompositeKey()
+        public void FindOnSeededData_WithCompositeKey_ReturnsValue()
         {
-            var builder = new Double<IMyContext>()
+            var builder = new ContextBuilder<IMyContext>()
                 .Setup(x => x.Authors, new List<Author>())
                 .Setup(x => x.Books, new List<Book>
                 {
                     new Book { Title = "Lotr", PublisherName = "pub1" ,ReleaseYear = 1954, AuthorId = 1},
                     new Book { Title = "HP", PublisherName = "pub1" ,ReleaseYear = 1997, AuthorId = 2}
                 });
-            var target = builder.Build();
+            var target = builder.GetContext();
 
             Assert.AreEqual(2, target.Books.Count());
 
@@ -71,62 +71,18 @@ namespace MockEF.Tests
         }
 
         [TestMethod]
-        public void DeleteData()
+        public void DeletedSeedData_StaysRemoved()
         {
-            var builder = new Double<IMyContext>()
-                .Setup(x => x.Authors, new List<Author> {new Author {Name = "bob"}})
+            var builder = new ContextBuilder<IMyContext>()
+                .Setup(x => x.Authors, new List<Author> { new Author { Name = "bob" } })
                 .Setup(x => x.Books, new List<Book>());
-            var target = builder.Build();
+            var target = builder.GetContext();
 
             Assert.AreEqual(1, target.Authors.Count());
             Assert.IsTrue(target.Authors.Single().Name == "bob");
 
             target.Authors.Remove(target.Authors.Single(x => x.Name == "bob"));
             Assert.AreEqual(0, target.Authors.Count());
-        }
-
-        [TestMethod]
-        public void Enumerator_IndexOf()
-        {
-            var author = new Author {Name = "bob"};
-            var builder = new Double<IMyContext>()
-                .Setup(x => x.Authors, new List<Author> { author })
-                .Setup(x => x.Books, new List<Book>());
-            var target = builder.Build();
-            
-            var result = target.Authors.ToList();
-
-            Assert.IsNotNull(result);
-            Assert.AreEqual(0, target.Authors.ToList().IndexOf(author));
-        }
-
-        [TestMethod]
-        public void ElementType_CanBeChecked()
-        {
-            var author = new Author { Name = "bob" };
-            var builder = new Double<IMyContext>()
-                .Setup(x => x.Authors, new List<Author> { author })
-                .Setup(x => x.Books, new List<Book>());
-            var target = builder.Build();
-
-            var result = target.Authors.ElementType;
-
-            Assert.IsNotNull(result);
-            Assert.AreEqual(typeof(Author), result);
-        }
-
-        [TestMethod]
-        public void CreateNewAndAttach()
-        {
-            var builder = new Double<IMyContext>()
-                .Setup(x => x.Authors)
-                .Setup(x => x.Books);
-            var target = builder.Build();
-
-            var newObj = target.Authors.Create();
-            target.Authors.Attach(newObj);
-
-            Assert.AreEqual(1, target.Authors.Count());
         }
     }
 }
